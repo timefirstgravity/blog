213a36f52da4edef99beb1c1ed9a2a529c678aa1
timefirstgravity <contact@timefirstgravity.com>
2025-09-23 07:59:22 -0700

    move papers to new page

--- serve.py
+++ serve.py
@@ -7,8 +7,24 @@ import http.server
 import socketserver
 import signal
 import sys
+import socket
 
 PORT = 8000
+
+class QuietHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    """HTTP request handler with suppressed broken pipe errors"""
+
+    def handle(self):
+        try:
+            super().handle()
+        except (BrokenPipeError, ConnectionResetError, socket.error) as e:
+            # Silently ignore broken pipe and connection reset errors
+            pass
+
+    def log_error(self, format, *args):
+        # Only log errors that aren't broken pipe related
+        if "Broken pipe" not in format % args:
+            super().log_error(format, *args)
 
 class ReuseAddrTCPServer(socketserver.TCPServer):
     allow_reuse_address = True
@@ -20,7 +36,7 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-Handler = http.server.SimpleHTTPRequestHandler
+Handler = QuietHTTPRequestHandler
 
 print(f"Server running at http://localhost:{PORT}/")
 print("Press Ctrl+C to stop\n")
